786a5363fae82c024d93630b3269ca4a3eecf2ed
Chandu2828 <purnachand7569@gmail.com>
2026-05-28 19:07:16 +0530

    Added files

--- 10-conditions.sh
+++ 10-conditions.sh
@@ -9,9 +9,9 @@ NUMBER=$1
 # ge - greater than or equal
 # le - less than or equal
 
-if [$NUMBER -gt 20]; then
+if [ $NUMBER -gt 20 ]; then
     echo "Given number $NUMBER is greater to 20"
-elif [$NUMBER -eq 20]; then
+elif [ $NUMBER -eq 20 ]; then
     echo "Given number $NUMBER is equal to 20"
 else 
     echo "Given number $NUMBER is less to 20"
