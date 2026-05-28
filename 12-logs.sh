9e33695fe761ff1158fd16a4a87cfe1f7288032e
Chandu2828 <purnachand7569@gmail.com>
2026-05-28 19:47:28 +0530

    Added files

---
+++ 12-logs.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+USERID=$(id -u)
+LOGS_DIR=/var/log/shell-script
+LOGS_FILE="$LOGS_DIR/$0.log" # /var/log/shell-script/10-logs.sh.log
+
+# check root access or not
+if [ $USERID -ne 0]; then
+    echo "Please run the script with root access"
+    exit 1
+fi
+
+# first arg --> What are you trying to install
+# second arg --> exit code
+VALIDATE(){
+    if [ $2 -ne 0 ]; then
+        echo "Installing $1 is ... FAILED" | tee -a $LOGS_FILE
+        exit 1
+    else    
+        echo "Installing $1 is ... SUCCESS" | tee -a $LOGS_FILE
+}
+
+dnf list installed mysql &>> $LOGS_FILE
+
+if [ $? -eq 0]; then
+    echo "MySQL is already installed ... SKIPPING" | tee -a $LOGS_FILE
+else
+    echo "Installing MySQL"
+    dnf install mysql -y &>> $LOGS_FILE
+    VALIDATE MySQL $?
+fi 
+
+dnf list installed nginx &>> $LOGS_FILE
+if [ $? -ne 0]; then
+    echo "nginx is already installed ... SKIPPING" | tee -a $LOGS_FILE
+else
+    echo "Installing nginx"
+    dnf install nginx -y &>> $LOGS_FILE
+    VALIDATE Nginx $?
+fi
